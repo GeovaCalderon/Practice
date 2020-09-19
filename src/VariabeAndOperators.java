cd5c04dedbf8015faf2ba6796f6ca9e807d5a9b9
Geovanni Calderon <geovanni.calderon@ulead.ac.cr>
2020-09-18 21:35:12 -0600

    commit inicial

---
+++ src/VariabeAndOperators.java
@@ -0,0 +1,18 @@
+public class VariabeAndOperators {
+
+
+    public int anothermethod(){
+        return 5;
+    }
+
+    public static void main(String[] args){
+
+        long longNumber = 0;
+        int numberOfStudents = 0;
+        short shortNumber = 1;
+
+        char letter = 'a';
+
+    }
+
+}
