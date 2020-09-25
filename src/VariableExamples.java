cecba93ba60e33de6e785b32ea555820fcf204bc
Geovanni Calderon <geovanni.calderon@ulead.ac.cr>
2020-09-24 22:30:30 -0600

    Entrega tarea 1

---
+++ src/VariableExamples.java
@@ -0,0 +1,19 @@
+public class VariableExamples {
+
+    public static void main(String[] args) {
+
+        //Una variable numérica de 16 bits e inicialícela con
+        // el máximo valor entero que este tipo de variable puede almacenar.
+
+        short variable1 = 32767;
+
+        //Una variable numérica de 64 bits e inicialícela con un valor que no podría ser almacenado
+        // en una variable entera de 32bits debido a su tamaño en memoria.
+
+        long variable2 = 2147483647;
+
+        //Una variable no entera de 64 bits e inicialícela con un valor que incluya decimales.
+
+        double variable3 = 245.16;
+    }
+}
